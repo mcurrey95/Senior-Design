c6d60969ed66e5547227207d1f9419b56ff25ea7
Grant <gjamespowell@gmail.com>
2016-11-13 16:06:46 -0500

    Added basic constraints

--- model.py
+++ model.py
@@ -1,1 +1,15 @@
 #model goes here
+
+
+import pulp
+
+prob = pulp.LpProblem("Truck Driver Scheduling", pulp.LpMinimize)
+
+NUMBER_OF_DRIVERS = 32
+
+DRIVERS = [i for i in range(NUMBER_OF_DRIVERS)] #TODO: MAKE A FUNCTION TO GET THIS NUMBER
+SHIFTS = [day for day in range(14)]
+
+
+# class pulp.LpVariable(name, lowBound=None, upBound=None, cat='Continuous', e=None)
+DecisionVariables = {i: {j: pulp.LpVariable('') for j in SHIFTS} for i in DRIVERS}
